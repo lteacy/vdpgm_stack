031d94da061a06d1ef9b172400182e364f7f6563
Luke Teacy <wtlt@ecs.soton.ac.uk>
2013-03-29 19:32:42 +0000

    Added skeleton data listener and reset service

--- vdpgm_srv/src/main/java/org/soton/vdpgm_srv/IMMServer.java
+++ vdpgm_srv/src/main/java/org/soton/vdpgm_srv/IMMServer.java
@@ -1,11 +1,15 @@
 package org.soton.vdpgm_srv;
 
+import org.apache.commons.logging.Log;
+import org.ros.message.MessageListener;
+import org.ros.node.topic.Subscriber;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 import org.ros.node.NodeMain;
 import org.ros.node.service.ServiceResponseBuilder;
 import org.ros.node.service.ServiceServer;
+import vdpgm_msgs.*;
 
 /**
  * This is a simple {@link ServiceServer} {@link NodeMain}.
@@ -24,7 +28,30 @@ public class IMMServer extends AbstractNodeMain
    @Override
    public void onStart(ConnectedNode node)
    {
-      node.newServiceServer("vdpgm/get_imm", vdpgm_msgs.GetModel._TYPE, new IMMResponder(node.getTopicMessageFactory()));
+      final Log log = node.getLog();
+      Subscriber<DataStamped> subscriber = node.newSubscriber("vdpgm/data", DataStamped._TYPE);
+      subscriber.addMessageListener(new MessageListener<DataStamped>()
+         {
+            @Override
+            public void onNewMessage(DataStamped message)
+            {
+               log.info("I heard: \"" + message.getData() + "\"");
+            }
+         });
+
+      node.newServiceServer("vdpgm/get_imm", GetModel._TYPE,
+            new IMMResponder(node.getTopicMessageFactory()));
+
+      node.newServiceServer("vdpgm/reset", Reset._TYPE,
+         new ServiceResponseBuilder<ResetRequest, ResetResponse>()
+         {
+            @Override
+            public void build(ResetRequest request, ResetResponse response)
+            {
+               // clear all previously received data
+               log.info("I've been asked to reset");
+            }
+         });
 
    } // method onStart
 
